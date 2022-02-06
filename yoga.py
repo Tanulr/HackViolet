48f8b5254cfa9dc9c0c0b1e6361eeaf0fe57893a
Tanulr <73035034+Tanulr@users.noreply.github.com>
2022-02-06 10:40:36 +0530

    Call poseConfirm() function
    Mediapipe. Warrior pose, T pose and Tree pose

--- yoga.py
+++ yoga.py
@@ -229,58 +229,61 @@ def classifyPose(landmarks, output_image, display=False):
         return output_image, label
 
 
-# Setup Pose function for video.
-pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=1)
+def poseConfirm():
+    # Setup Pose function for video.
+    pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=1)
 
-# Initialize the VideoCapture object to read from the webcam.
-camera_video = cv2.VideoCapture(0)
+    # Initialize the VideoCapture object to read from the webcam.
+    camera_video = cv2.VideoCapture(0)
 
-# Initialize a resizable window.
-cv2.namedWindow('Pose Classification', cv2.WINDOW_NORMAL)
+    # Initialize a resizable window.
+    cv2.namedWindow('Pose Classification', cv2.WINDOW_NORMAL)
 
-# Iterate until the webcam is accessed successfully.
-while camera_video.isOpened():
-    
-    # Read a frame.
-    ok, frame = camera_video.read()
-    
-    # Check if frame is not read properly.
-    if not ok:
+    # Iterate until the webcam is accessed successfully.
+    while camera_video.isOpened():
         
-        # Continue to the next iteration to read the next frame and ignore the empty camera frame.
-        continue
-    
-    # Flip the frame horizontally for natural (selfie-view) visualization.
-    frame = cv2.flip(frame, 1)
-    
-    # Get the width and height of the frame
-    frame_height, frame_width, _ =  frame.shape
-    
-    # Resize the frame while keeping the aspect ratio.
-    frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
-    
-    # Perform Pose landmark detection.
-    frame, landmarks = detectPose(frame, pose_video, display=False)
-    
-    # Check if the landmarks are detected.
-    if landmarks:
+        # Read a frame.
+        ok, frame = camera_video.read()
         
-        # Perform the Pose Classification.
-        frame, _ = classifyPose(landmarks, frame, display=False)
-    
-    # Display the frame.
-    cv2.imshow('Pose Classification', frame)
-    
-    # Wait until a key is pressed.
-    # Retreive the ASCII code of the key pressed
-    k = cv2.waitKey(1) & 0xFF
-    
-    # Check if 'ESC' is pressed.
-    if(k == 27):
+        # Check if frame is not read properly.
+        if not ok:
+            
+            # Continue to the next iteration to read the next frame and ignore the empty camera frame.
+            continue
         
-        # Break the loop.
-        break
+        # Flip the frame horizontally for natural (selfie-view) visualization.
+        frame = cv2.flip(frame, 1)
+        
+        # Get the width and height of the frame
+        frame_height, frame_width, _ =  frame.shape
+        
+        # Resize the frame while keeping the aspect ratio.
+        frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
+        
+        # Perform Pose landmark detection.
+        frame, landmarks = detectPose(frame, pose_video, display=False)
+        
+        # Check if the landmarks are detected.
+        if landmarks:
+            
+            # Perform the Pose Classification.
+            frame, _ = classifyPose(landmarks, frame, display=False)
+        
+        # Display the frame.
+        cv2.imshow('Pose Classification', frame)
+        
+        # Wait until a key is pressed.
+        # Retreive the ASCII code of the key pressed
+        k = cv2.waitKey(1) & 0xFF
+        
+        # Check if 'ESC' is pressed.
+        if(k == 27):
+            
+            # Break the loop.
+            break
 
-# Release the VideoCapture object and close the windows.
-camera_video.release()
-cv2.destroyAllWindows()
+    # Release the VideoCapture object and close the windows.
+    camera_video.release()
+    cv2.destroyAllWindows()
+
+#poseConfirm()
